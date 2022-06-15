1de7ddc502b8ad188d61cf608558423f63387d0d
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-06-15 17:42:43 +0200

    impostato header

--- src/App.vue
+++ src/App.vue
@@ -22,5 +22,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+  @import '../src/assets/style/global.scss'
 </style>
--- src/components/HeaderComponents.vue
+++ src/components/HeaderComponents.vue
@@ -1,5 +1,22 @@
 <template>
-  <header></header>
+  <header class="header-background">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-3 header-text">
+          <i class="fa-solid fa-clock"></i> Open Hours: Mon - Sat - 9:00 - 18:00
+        </div>
+        <div class="col-6 header-text d-flex">
+          <div class="px-3"><i class="fa-solid fa-clock"></i> +1 (305) 1234-5678</div>
+          <div class="px-3"><i class="fa-solid fa-clock"></i> hello@examplel.com</div>
+          <div class="px-3">
+            <i class="fa-solid fa-clock"></i>
+            <i class="fa-solid fa-clock"></i>
+            <i class="fa-solid fa-clock"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,5 +26,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.header-background{
+  background-color: var(--primary-color);
+  height: 40px;
+}
 </style>
