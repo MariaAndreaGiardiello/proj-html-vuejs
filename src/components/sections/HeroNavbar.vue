e048cc411b5d10352bf0fdff45f72b46134a7620
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-06-16 17:36:54 +0200

    corretto errori e aggiunto elementi prima sezione

--- src/components/sections/HeroNavbar.vue
+++ src/components/sections/HeroNavbar.vue
@@ -1,29 +1,41 @@
 <template>
     <section class="first-hero">
+            <!-- Hero + Logo -->
         <div class="container-md">
-            <div class="row justify-content-between mt-3">
-            <div class="col-2 d-flex">
-                <div class="first-part-logo">
-                    <div class="logo-green-text">NEX</div>
+            <div class="row justify-content-between">
+                <div class="col-2 d-flex align-items-center">
+                    <div class="first-part-logo">
+                        <div class="logo-green-text">NEX</div>
+                    </div>
+                    <div class="second-part-logo">
+                        <div class="logo-white-text">GEN</div>
+                    </div>
                 </div>
-                <div class="second-part-logo">
-                    <div class="logo-white-text">GEN</div>
-                </div>
-            </div>
+                <!-- /Hero + Logo -->
 
-            <div class="col-6">
-                <nav>
-                    <ul class="d-flex">
-                        <li class="px-2"><a href=""></a>HOME</li>
-                        <li class="px-2"><a href=""></a>HOME</li>
-                        <li class="px-2"><a href=""></a>HOME</li>
-                        <li class="px-2"><a href=""></a>HOME</li>
-                        <li class="px-2"><a href=""></a>HOME</li>
-                        <li class="px-2"><button></button></li>
+                <!-- Navbar -->
+                <nav class="col-6">
+                    <ul class="d-flex align-items-center">
+                        <li class="navbar-text px-2"><a href=""></a>HOME</li>
+                        <li class="px-2 navbar-text"><a href=""></a>HOME</li>
+                        <li class="px-2 navbar-text"><a href=""></a>HOME</li>
+                        <li class="px-2 navbar-text"><a href=""></a>HOME</li>
+                        <li class="px-2 navbar-text"><a href=""></a>HOME</li>
+                        <li class="px-2 navbar-text"><button></button></li>
                     </ul>
                 </nav>
+                <!-- /Navbar -->
             </div>
-        </div>
+            <div class="row justify-content-center">
+                <div class="col-6 justify-content-center">
+                    <h1>Lead ~ Customer</h1>
+                    <p>Successful strategies require data analysis, creativity and customer focus, taking advantage of the latest technologies to offer excellent services.</p>
+                    <div class="col-4">
+                        <button></button>
+                        <button></button>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
