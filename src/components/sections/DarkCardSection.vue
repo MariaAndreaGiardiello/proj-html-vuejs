2b0c729358571606491621227d1aef0a4c7db6de
MariaAndreaGiardiello <giardiello.m.a@icloud.com>
2022-06-17 16:20:25 +0200

    aggiunto stile light-card

--- src/components/sections/DarkCardSection.vue
+++ src/components/sections/DarkCardSection.vue
@@ -1,5 +1,56 @@
 <template>
-  <section></section>
+    <section class="section-background d-flex align-items-center">
+        <div class="container-sm justify-content-center ">
+            <div class="row justify-content-center d-grid gap-3">
+                <div class="col-12"><h6>OUR BUSINESS</h6></div>
+                <div class="col-12"><h2>Excellence in <span>Services</span></h2></div>
+                <div class="row justify-content-between mb-5">
+                    <div class="col-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet nobis blanditiis, aliquam fuga quos commodi porro laboriosam nostrum beatae quam laudantium numquam odit. Vel enim adipisci nobis harum numquam suscipit.</div>
+                    <div class="col-2"><button type="button" class="transparent-button">SEE ALL</button></div>
+                </div>
+                <div class="col-3 mx-2 gray-box">
+                    <h4>Human Capital</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                    <div class="col-12"><button type="button" class="transparent-button">SEE ALL</button></div>
+                </div>
+                <div class="col-3 mx-2 gray-box">
+                    <h4>Human Capital</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                    <div class="col-12">
+                        <button type="button" class="transparent-button">SEE ALL</button>
+                    </div>
+                </div>
+                <div class="col-3 mx-2 gray-box">
+                    <h4>Human Capital</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                    <div class="col-12">
+                        <button type="button" class="transparent-button">SEE ALL</button>
+                    </div>
+                </div>
+                <div class="col-3 mx-2 gray-box">
+                    <h4>Human Capital</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                    <div class="col-12">
+                        <button type="button" class="transparent-button">SEE ALL</button>
+                    </div>
+                </div>
+                <div class="col-3 mx-2 gray-box">
+                    <h4>Human Capital</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                    <div class="col-12">
+                        <button type="button" class="transparent-button">SEE ALL</button>
+                    </div>
+                </div>
+                <div class="col-3 mx-2 gray-box">
+                    <h4>Human Capital</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                    <div class="col-12">
+                        <button type="button" class="transparent-button">SEE ALL</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -9,5 +60,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.section-background {
+    background-color: black;
+    padding: 50px 0;
+}
 
+.gray-box{
+    padding: 20px;
+    background-color: rgb(25, 25, 30);
+    border-radius: 15px;
+    margin: 10px;
+    color: white;
+}
 </style>
--- src/components/sections/LightCardSection.vue
+++ src/components/sections/LightCardSection.vue
@@ -1,23 +1,25 @@
 <template>
   <section class="section-background d-flex align-items-center">
-    <div class="container-sm justify-content-center ">
-        <div class="row justify-content-center d-grid gap-3">
-            <div class="col-3 mx-2 white-box">
-                <h4>Human Capital</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
-            </div>
-            <div class="col-3 mx-2 white-box">
-                <h4>Human Capital</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
-            </div>
-            <div class="col-3 mx-2 white-box">
-                <h4>Human Capital</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+        <div class="container-sm justify-content-center ">
+            <div class="row justify-content-center d-grid gap-3">
+                <div class="col-3 mx-2 white-box">
+                    <div class="img-circle"><img class="img-fluid" src="src/assets/img/users-icon.png" alt=""></div>
+                    <h5 class="card-title">Human Capital</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                </div>
+                <div class="col-3 mx-2 white-box">
+                    <div class="img-circle"><img class="img-fluid" src="src/assets/img/users-icon.png" alt=""></div>
+                    <h5 class="card-title">Core Business</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                </div>
+                <div class="col-3 mx-2 white-box">
+                    <div class="img-circle"><img class="img-fluid" src="src/assets/img/users-icon.png" alt=""></div>
+                    <h5 class="card-title">Performance</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint saepe quibusdam in similique at doloremque</p>
+                </div>
             </div>
         </div>
-    </div>
-
-  </section>
+    </section>
 </template>
 
 <script>
@@ -30,10 +32,31 @@ export default {
 .white-box{
     background-color: white;
     border-radius: 10px;
+    padding: 20px;
 }
 
 .section-background {
     background-color: var(--tertiary-color);
     height: 400px;
+}
+
+.img-circle{
+    width: 40px;
+    height: 40px;
+    border-radius: 100%;
+    background-color: var(--circle-icon);
+    margin-bottom: 15px;
+    opacity: 0.7
+    ;
+}
+
+.card-title{
+    font-weight: bold;
+    font-size: 16px;
+}
+
+.card-text{
+    font-size: 12px;
+    color: var(--font-color);
 }
 </style>
